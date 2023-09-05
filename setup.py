7d464d25ba27239f3af2d614620e8dc4090c85bd
john <vus.back@alpteam.one>
2023-09-05 17:07:30 +0300

    [MOD] version for pip

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='alp_api',
-    packages=find_packages(include=['alp_api']),
+    name='alpcom_api',
+    packages=find_packages(include=['alpcom_api']),
     install_requires=[
         'requests==2.31.0',
         'pydantic==2.1.1',
     ],
     version='1.0.0',
     description='ALP.com API Python',
-    author='John Petrucci',
+    author='ALP.com',
+    license='MIT'
 )
